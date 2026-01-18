2cfdd9732c3b214639b6a35021d0846a7d1cc6fd
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-18 22:16:04 +0530

    Basics 01

---
+++ Basics/01-index.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+console.log("Pratik 1")
+console.log("Pratik 2")
+
+// datatypes : string, number , boolean  
+let fname = "Pratik";
+let lname = "Gaikwad";
+let fullname = fname + " " + lname
+console.log(fullname)
