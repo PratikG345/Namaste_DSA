4598775093b862fd679fa1e8ba58ca4399c7967d
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-24 23:45:56 +0530

    rectangle pattern

--- Star Patterns/pattern1.js
+++ Star Patterns/pattern1.js
@@ -2,10 +2,7 @@ let n=5
 for(let i=0;i<n;i++){
     row = " ";
     for(let j=0;j<n;j++){
-        if((i==0 || i ==n) || (j==1 || j==n)){
-            row+="x ";
-        }
-
+        row+="x ";
     }
     console.log(row)
 }
