36d9abafb62d20bc62e6f940f0ccc77a6738ed04
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-26 18:36:03 +0530

    triangle_pattern *

---
+++ Star Patterns/pattern2.js
@@ -0,0 +1,17 @@
+/*
+
+    *
+    * *
+    * * *
+    * * * *
+
+*/
+
+let n=4;
+for(let i=0;i<n;i++){
+    row=" ";
+    for(let j=0;j<i+1;j++){
+        row=row+"* ";
+    }
+    console.log(row)
+}
