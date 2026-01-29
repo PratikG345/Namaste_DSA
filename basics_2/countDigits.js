dfba02e9beeb7d0919d3c0323a51e65201b66aa1
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-29 16:36:35 +0530

    count digits code

---
+++ basics_2/countDigits.js
@@ -0,0 +1,18 @@
+function countDigits(num){
+
+    // if(num==0) return 1
+
+    // num = Math.abs(num)
+    let count = 0
+    while(num>0){
+        num = Math.floor(num/10)
+        count++
+    }
+    return count
+}
+
+console.log(countDigits(0))
+
+// let n = ["Pratik","Soham"]
+// console.log(n[0][2])
+// console.log(n[1][3])
