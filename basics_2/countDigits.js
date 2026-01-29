a7e9083f325c713897633210063668a3600caf2b
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-29 16:37:11 +0530

    count digits code with -ve numbers and zero edge case

--- basics_2/countDigits.js
+++ basics_2/countDigits.js
@@ -1,8 +1,8 @@
 function countDigits(num){
 
-    // if(num==0) return 1
+    if(num==0) return 1
 
-    // num = Math.abs(num)
+    num = Math.abs(num)
     let count = 0
     while(num>0){
         num = Math.floor(num/10)
