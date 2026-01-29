933176f31ee3178ef0f242caa8ea6c0d1c55d183
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-29 17:19:17 +0530

    added definations of 4 functions

--- basics_2/countDigits.js
+++ basics_2/countDigits.js
@@ -1,8 +1,8 @@
 function countDigits(num){
-
+    
     if(num==0) return 1
 
-    num = Math.abs(num)
+    num = Math.abs(num) 
     let count = 0
     while(num>0){
         num = Math.floor(num/10)
@@ -11,4 +11,8 @@ function countDigits(num){
     return count
 }
 
+// abs = absolute,changes -ve to +ve number, keeps +ve number +ve.
+// ceil: Rounds up to the nearest integer.
+// round: Rounds to the nearest integer.
+// floor: Rounds down to the nearest integer.
 console.log(countDigits(0))
